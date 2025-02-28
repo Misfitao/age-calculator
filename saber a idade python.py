e5fdbc41561bf4dd6c9ecf0760bb7fe4d77ea866
Misfitao <doylemisfits138@gmail.com>
2025-02-28 19:30:46 -0300

    Add files via upload

---
+++ saber a idade python.py
@@ -0,0 +1,7 @@
+print("hello")
+name = input("what is your name ? ")
+print(" hello " +name)
+born = int(input(" which year did you born in ? "))
+year = int(input("which year you are now ? "))
+age = int(year) - int(born)
+print("your current age is  " + str(age))
